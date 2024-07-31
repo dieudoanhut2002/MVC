fc0c82fb6fb2e1a8f3f34d0ff6bb29a6ed8a929b
dieudoanhut2002 <127190623+dieudoanhut2002@users.noreply.github.com>
2024-07-31 11:10:29 +0700

    Add MyDemoMVC project

---
+++ MyDeMoMVC/MyDeMoMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyDeMoMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
