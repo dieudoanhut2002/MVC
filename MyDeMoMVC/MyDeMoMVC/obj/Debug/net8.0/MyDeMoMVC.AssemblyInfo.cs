bc7745c31a1b11b442a0516b795cf7f81da726cd
dieudoanhut2002 <127190623+dieudoanhut2002@users.noreply.github.com>
2024-07-31 11:26:22 +0700

    add MenuTop folder

--- MyDeMoMVC/MyDeMoMVC/obj/Debug/net8.0/MyDeMoMVC.AssemblyInfo.cs
+++ MyDeMoMVC/MyDeMoMVC/obj/Debug/net8.0/MyDeMoMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyDeMoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02b8c4f7300a1445d4f388c132bf16bb32493811")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+496a2c7f5d8c78b41984e0c736b6c9cb82090ba2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyDeMoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyDeMoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
